21609ba5f0361249e2abe402297ccd2e43c5d950
Dominik Steinberger <dominik.steinberger@fau.de>
2016-07-12 11:12:18 +0200

    Setup package structure.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+import re
+import ast
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open(os.path.join(root_dir, 'geometry', '__init__.py'), 'r') as f:
+    version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
+
+setup(
+    name='geometry',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/CompMaterSci/python-geometry',
+    license='BSD',
+    author='Stefan Sandfeld',
+    author_email='stefan.sandfeld@fau.de',
+    description='Dealing with geometrical problems using Python.'
+)
