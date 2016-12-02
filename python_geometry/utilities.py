954357e078bb2fd11c678d9f6769a0b964b800be
Dominik Steinberger <ZeeD26@users.noreply.github.com>
2016-12-02 11:59:33 +0100

    Add intersection of a bound vector with a plane. (#14)

--- python_geometry/utilities.py
+++ python_geometry/utilities.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-from numpy import allclose, asanyarray, cross, mean, zeros
+from __future__ import division
+
+from numpy import allclose, asanyarray, cross, dot, mean, zeros
 from numpy.linalg import norm
+
+from .bound_vector import BoundVector
+from .plane import Plane
 
 
 def get_normal_vector(points):
@@ -12,3 +17,38 @@ def get_normal_vector(points):
     if allclose(normal_vector, 0):
         return zeros(points.shape[1])
     return normal_vector/norm(normal_vector)
+
+
+def get_intersection(object_0, object_1):
+    if isinstance(object_0, BoundVector) and isinstance(object_1, Plane):
+        intersection = _get_intersection_bound_vector_plane(
+            bound_vector=object_0,
+            plane=object_1)
+    elif isinstance(object_0, Plane) and isinstance(object_1, BoundVector):
+        intersection = _get_intersection_bound_vector_plane(
+            bound_vector=object_1,
+            plane=object_0)
+    else:
+        raise NotImplementedError(
+            'Intersection of a {} and a {} is not yet implemented.'.format(
+                object_0.__class__.__name__,
+                object_1.__class__.__name__))
+    return intersection
+
+
+def _get_intersection_bound_vector_plane(bound_vector, plane):
+
+    param = (
+        dot(
+            plane.point_in_plane - bound_vector.initial_point,
+            plane.normal_vector) /
+        dot(
+            bound_vector.terminal_point - bound_vector.initial_point,
+            plane.normal_vector))
+    if 0 <= param <= 1:
+        intersection = (
+            bound_vector.initial_point +
+            param*(bound_vector.terminal_point - bound_vector.initial_point))
+    else:
+        intersection = None
+    return intersection
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import pytest
 from numpy.testing import assert_allclose
+from numpy import array
 
-from python_geometry.utilities import get_normal_vector
+from python_geometry.utilities import get_intersection, get_normal_vector
+from python_geometry.plane import Plane
+from python_geometry.bound_vector import BoundVector
 
 
 class TestGetNormalVector:
@@ -50,3 +54,97 @@ class TestGetNormalVector:
             (1, 1, 1)
         ]
         assert_allclose(get_normal_vector(points), (0, 0, 0))
+
+
+class TestGetIntersection(object):
+
+    def test_PlaneThatIntersectsBoundVectorIntersection_ReturnPoint_0(self):
+        plane = Plane(
+            point_in_plane=array([0, 0, 0]),
+            normal_vector=array([1, 0, 0]))
+        bound_vector = BoundVector(
+            initial_point=array([-1, 0, 0]),
+            terminal_point=array([1, 0, 0]))
+        actual = get_intersection(plane, bound_vector)
+        expected = array([0, 0, 0])
+        assert_allclose(actual, expected)
+
+    def test_PlaneThatIntersectsBoundVectorIntersection_ReturnPoint_1(self):
+        plane = Plane(
+            point_in_plane=array([1, 1, 1]),
+            normal_vector=array([1, 0, 0]))
+        bound_vector = BoundVector(
+            initial_point=array([-1, 0, 0]),
+            terminal_point=array([1, 0, 0]))
+        actual = get_intersection(plane, bound_vector)
+        expected = array([1, 0, 0])
+        assert_allclose(actual, expected)
+
+    def test_PlaneThatDoesNotIntersectBoundVectorIntersection_ReturnNone_0(self):
+        plane = Plane(
+            point_in_plane=array([0, 0, 0]),
+            normal_vector=array([1, 0, 0]))
+        bound_vector = BoundVector(
+            initial_point=array([1, 0, 0]),
+            terminal_point=array([2, 0, 0]))
+        assert get_intersection(plane, bound_vector) is None
+
+    def test_PlaneThatDoesNotIntersectBoundVectorIntersection_ReturnNone_1(self):
+        plane = Plane(
+            point_in_plane=array([0, 0, 0]),
+            normal_vector=array([1, 0, 0]))
+        bound_vector = BoundVector(
+            initial_point=array([-2, 0, 0]),
+            terminal_point=array([-1, 0, 0]))
+        assert get_intersection(plane, bound_vector) is None
+
+    def test_BoundVectorThatIntersectsPlaneIntersection_ReturnPoint_0(self):
+        bound_vector = BoundVector(
+            initial_point=array([-1, 0, 0]),
+            terminal_point=array([1, 0, 0]))
+        plane = Plane(
+            point_in_plane=array([0, 0, 0]),
+            normal_vector=array([1, 0, 0]))
+        actual = get_intersection(bound_vector, plane)
+        expected = array([0, 0, 0])
+        assert_allclose(actual, expected)
+
+    def test_BoundVectorThatIntersectsPlaneIntersection_ReturnPoint_1(self):
+        bound_vector = BoundVector(
+            initial_point=array([-1, 0, 0]),
+            terminal_point=array([1, 0, 0]))
+        plane = Plane(
+            point_in_plane=array([1, 1, 1]),
+            normal_vector=array([1, 0, 0]))
+        actual = get_intersection(bound_vector, plane)
+        expected = array([1, 0, 0])
+        assert_allclose(actual, expected)
+
+    def test_BoundVectorThatDoesNotIntersectPlaneIntersection_ReturnNone_0(self):
+        bound_vector = BoundVector(
+            initial_point=array([1, 0, 0]),
+            terminal_point=array([2, 0, 0]))
+        plane = Plane(
+            point_in_plane=array([0, 0, 0]),
+            normal_vector=array([1, 0, 0]))
+        assert get_intersection(bound_vector, plane) is None
+
+
+    def test_BoundVectorThatDoesNotIntersectPlaneIntersection_ReturnNone_1(self):
+        bound_vector = BoundVector(
+            initial_point=array([-2, 0, 0]),
+            terminal_point=array([-1, 0, 0]))
+        plane = Plane(
+            point_in_plane=array([0, 0, 0]),
+            normal_vector=array([1, 0, 0]))
+        assert get_intersection(bound_vector, plane) is None
+
+    def test_BoundVectorBoundVectorIntersection_RaiseNotImplementedError(self):
+        bound_vector_0 = BoundVector(
+            initial_point=array([-1, 0, 0]),
+            terminal_point=array([1, 0, 0]))
+        bound_vector_1 = BoundVector(
+            initial_point=array([-1, 0, 0]),
+            terminal_point=array([1, 0, 0]))
+        with pytest.raises(NotImplementedError):
+            get_intersection(bound_vector_0, bound_vector_1)
